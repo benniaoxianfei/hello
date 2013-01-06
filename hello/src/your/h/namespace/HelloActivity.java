d4cc012b43748b9482527f1e82d363207144f60e
benniaoxianfei <xiaomeng.su@dragonflow.com>
2013-01-06 11:59:32 +0800

    dddd

--- hello/src/your/h/namespace/HelloActivity.java
+++ hello/src/your/h/namespace/HelloActivity.java
@@ -9,5 +9,12 @@ public class HelloActivity extends Activity {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
+        
+        
+        
+        
+        
+        
+        
     }
 }
